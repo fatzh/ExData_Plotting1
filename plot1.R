ce6aee6f2330e9cbb241042389d9a3c1763a6694
fatz <fabrice.tz@gmail.com>
2015-06-02 14:16:36 +0200

    initial commit

---
+++ plot1.R
@@ -0,0 +1,34 @@
+##
+## Loading the data
+##
+## The data file is assumed to be present in a subdirectory of the working directory called 'data'
+##
+if(!require(tidyr, quietly=TRUE)) {
+    stop("The package 'tidyr' is required, please install it to run this script.")
+}
+if(!require(sqldf, quietly=TRUE)) {
+    stop("The package 'sqldf' is required, please install it to run this script.")
+}
+
+## load data
+d <- read.csv.sql('./data/household_power_consumption.txt',
+                  sql="select * from file where Date in ('1/2/2007', '2/2/2007')",
+                  colClasses = c('character', 'character', rep('numeric', 7)),
+                  header=TRUE,
+                  sep=';',
+                  row.names=FALSE)
+
+
+## merge date and time
+d <- unite(d, Datetime, Date, Time, sep=' ')
+## and make it a proper time
+d$Datetime <- strptime(d$Datetime, "%d/%m/%Y %H:%M:%S")
+
+## write output in a PNG file
+png(filename="./plot1.png")
+
+## Plot 1
+hist(d$Global_active_power, col="red", xlab="Global Active Power (kilowatts)", main="Global Active Power")
+
+## close PNG file
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,36 @@
+##
+## Loading the data
+##
+## The data file is assumed to be present in a subdirectory of the working directory called 'data'
+##
+if(!require(tidyr, quietly=TRUE)) {
+    stop("The package 'tidyr' is required, please install it to run this script.")
+}
+if(!require(sqldf, quietly=TRUE)) {
+    stop("The package 'sqldf' is required, please install it to run this script.")
+}
+
+## load data
+d <- read.csv.sql('./data/household_power_consumption.txt',
+                  sql="select * from file where Date in ('1/2/2007', '2/2/2007')",
+                  colClasses = c('character', 'character', rep('numeric', 7)),
+                  header=TRUE,
+                  sep=';',
+                  row.names=FALSE)
+
+
+## merge date and time
+d <- unite(d, Datetime, Date, Time, sep=' ')
+## and make it a proper time
+d$Datetime <- strptime(d$Datetime, "%d/%m/%Y %H:%M:%S")
+
+## write output in a PNG file
+png(filename="./plot3.png")
+
+plot(rep(d$Datetime, 3), c(d$Sub_metering_1, d$Sub_metering_2, d$Sub_metering_3), type='n', ylab="Energy sub metering", xlab='')
+lines(d$Datetime, d$Sub_metering_1)
+lines(d$Datetime, d$Sub_metering_2, col="red")
+lines(d$Datetime, d$Sub_metering_3, col="blue")
+legend('topright', lwd=1,  col=c('black', 'red', 'blue'), legend=c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'))
+
+dev.off()
